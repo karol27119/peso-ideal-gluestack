3ab267ae164651ed0e676ee57d1a52c9cdd9586e
karol27119 <116905552+karol27119@users.noreply.github.com>
2023-12-06 17:44:57 +0000

    peso-ideal-gluestack

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { GluestackUIProvider, Text, Box, Center, Heading, FormControl, FormControlLabel, FormControlLabelText, Input, InputField, HStack, Button, ButtonText } from "@gluestack-ui/themed"
+import { GluestackUIProvider, Text, Box, Image, Center, Heading, FormControl, FormControlLabel, FormControlLabelText, Input, InputField, HStack, Button, ButtonText } from "@gluestack-ui/themed"
 import { config } from "@gluestack-ui/config"
 import { Switch } from 'react-native';
 import { useState } from "react";
 
+import homem from "./assets/homem.png"
+import mulher from "./assets/mulher.png"
 
 export default function Main() {
   const [peso, setPeso] = useState("72")
@@ -45,30 +47,31 @@ export default function Main() {
           <FormControlLabel>
             <FormControlLabelText color="#D16014" >Altura</FormControlLabelText>
           </FormControlLabel>
-          <Input w={"$full"} borderColor="#313715">
+          <Input w={"$full"} borderColor="#313715" >
             <InputField value={altura} onChangeText={setAltura} keyboardType="numeric" color="#313715"/>
           </Input>
-          <FormControlLabel>
-            <FormControlLabelText>Sexo</FormControlLabelText>
+          <FormControlLabel >
+            <FormControlLabelText color="#D16014">Sexo</FormControlLabelText>
           </FormControlLabel>
           <HStack width={"$full"} space="md" justifyContent="center" alignItems="center">
             <FormControlLabelText>Homem</FormControlLabelText>
             <Switch value={sexo} onValueChange={setSexo} />
             <FormControlLabelText>Mulher</FormControlLabelText>
           </HStack>
-          <Box justifyContent="space-around" display="flex" flexDirection="row">
-            <Button onPress={calcularHandle}>
+          <Box justifyContent="space-around" display="flex" flexDirection="row" >
+            <Button onPress={calcularHandle} bg="#D16014">
               <ButtonText>Calcular</ButtonText>
             </Button>
-            <Button onPress={limparHandle}>
+            <Button onPress={limparHandle} bg="#D16014">
               <ButtonText>Limpar</ButtonText>
             </Button>
           </Box>
           <Box justifyContent="space-around" alignItems="center" height={100} flexDirection="row" display="flex">
-            <Text>Resultado:</Text>
+            <Text color="#D16014">Resultado:</Text>
             <Text fontWeight="$bold">{resultado}</Text>
           </Box>
         </FormControl>
+        <Image source={sexo ? mulher : homem} height={'40%'} w={'$80%'} alt="imagem do tipo do sexo"/>
       </Box>
     </GluestackUIProvider>
   )
